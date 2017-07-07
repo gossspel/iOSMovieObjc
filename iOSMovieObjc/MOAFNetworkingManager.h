b31ad5dfcc1169e18cfa4340aee7b0acad96e6c6
Sunny Chan <sunnyucb@gmail.com>
2017-07-07 11:39:54 -0700

    Added MOAFNetworkingManager.

---
+++ iOSMovieObjc/MOAFNetworkingManager.h
@@ -0,0 +1,13 @@
+//
+//  MOAFNetworkingManager.h
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/7/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOAFNetworkingManager : NSObject
+
+@end
---
+++ iOSMovieObjc/MOAFNetworkingManager.m
@@ -0,0 +1,13 @@
+//
+//  MOAFNetworkingManager.m
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/7/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import "MOAFNetworkingManager.h"
+
+@implementation MOAFNetworkingManager
+
+@end
