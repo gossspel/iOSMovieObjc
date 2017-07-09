841e96ccea955b77f75c4fcbf4fd5900b6aaf00c
Sunny Chan <sunnyucb@gmail.com>
2017-07-09 10:12:13 -0700

    Added MOAPIClient.

---
+++ iOSMovieObjc/MOAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  MOAPIClient.h
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/9/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOAPIClient : NSObject
+
+@end
---
+++ iOSMovieObjc/MOAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  MOAPIClient.m
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/9/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import "MOAPIClient.h"
+
+@implementation MOAPIClient
+
+@end
