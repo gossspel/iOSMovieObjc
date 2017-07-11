cbbf25e6d70c7002b685582151bc43353bf0e8ea
Sunny Chan <sunnyucb@gmail.com>
2017-07-11 08:13:54 -0700

    Added empty MovieDataService.

---
+++ iOSMovieObjc/MovieDataService.h
@@ -0,0 +1,13 @@
+//
+//  MovieDataService.h
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/11/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovieDataService : NSObject
+
+@end
---
+++ iOSMovieObjc/MovieDataService.m
@@ -0,0 +1,13 @@
+//
+//  MovieDataService.m
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/11/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import "MovieDataService.h"
+
+@implementation MovieDataService
+
+@end
