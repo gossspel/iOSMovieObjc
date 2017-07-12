09b66d7d0aa97c56c40df7af1ec9fc067386d1f7
Sunny Chan <sunnyucb@gmail.com>
2017-07-12 12:57:57 -0700

    Added initial MovieListVM.

---
+++ iOSMovieObjc/MovieListVM.h
@@ -0,0 +1,13 @@
+//
+//  MovieListVM.h
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/12/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovieListVM : NSObject
+
+@end
---
+++ iOSMovieObjc/MovieListVM.m
@@ -0,0 +1,13 @@
+//
+//  MovieListVM.m
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/12/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import "MovieListVM.h"
+
+@implementation MovieListVM
+
+@end
