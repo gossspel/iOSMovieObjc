81c04a9a639720925d7e974ca07ffaea16254a13
Sunny Chan <sunnyucb@gmail.com>
2017-07-12 08:37:47 -0700

    Finished initial DataService.

--- iOSMovieObjc/MovieDataService.h
+++ iOSMovieObjc/MovieDataService.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, MovieDataServiceType) {
+    now_playing,
+    popular,
+    top_rated,
+    upcoming
+};
+
 @interface MovieDataService : NSObject
+
+- (instancetype _Nonnull)initWith:(MovieDataServiceType)serviceType;
+- (void)getListWithPage:(NSInteger)page
+      completionHandler:(void (^ _Nullable)(NSURLResponse * _Nonnull, id _Nullable, NSError * _Nullable))completionHandler;
 
 @end
--- iOSMovieObjc/MovieDataService.m
+++ iOSMovieObjc/MovieDataService.m
@@ -7,7 +7,51 @@
 //
 
 #import "MovieDataService.h"
+#import "MOAPIClient.h"
+
+@interface MovieDataService ()
+
+@property (nonatomic) MovieDataServiceType serviceType;
+@property (nonatomic, strong) MOAPIClient *apiClient;
+@property (nonatomic, strong) NSString *URLStr;
+
+@end
 
 @implementation MovieDataService
+
+- (instancetype)initWith:(MovieDataServiceType)serviceType {
+    self = [super init];
+    self.serviceType = serviceType;
+    self.apiClient = [[MOAPIClient alloc] init];
+    
+    NSString *baseURL = @"https://api.themoviedb.org/3/movie/";
+    NSString *partialURL = @"";
+    
+    switch (serviceType) {
+        case now_playing:
+            partialURL = @"now_playing";
+            break;
+        case popular:
+            partialURL = @"popular";
+            break;
+        case top_rated:
+            partialURL = @"top_rated";
+            break;
+        case upcoming:
+            partialURL = @"upcoming";
+            break;
+    }
+    
+    self.URLStr = [NSString stringWithFormat:@"%@%@", baseURL, partialURL];
+    
+    return self;
+}
+
+- (void)getListWithPage:(NSInteger)page
+      completionHandler:(void (^)(NSURLResponse * _Nonnull, id _Nullable, NSError * _Nullable))completionHandler
+{
+    NSDictionary<NSString *, NSString *> *params = @{@"page": [@(page) stringValue]};
+    [self.apiClient sendHTTPGETRequestWithURLStr:self.URLStr headers:nil queryStringParams:params completionHandler:completionHandler];
+}
 
 @end
