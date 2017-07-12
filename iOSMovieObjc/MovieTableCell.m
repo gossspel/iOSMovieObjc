c1618ebcba431828cbc33e2dd145f6f4c07e358d
Sunny Chan <sunnyucb@gmail.com>
2017-07-12 12:45:38 -0700

    Added MovieTableCell.

---
+++ iOSMovieObjc/MovieTableCell.m
@@ -0,0 +1,24 @@
+//
+//  MovieTableCell.m
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/12/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import "MovieTableCell.h"
+
+@implementation MovieTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
