b1574672ff250d5cfdcf9ff5a5b4b05807800fe0
Sunny Chan <sunnyucb@gmail.com>
2017-07-12 09:55:27 -0700

    Added MovieListTVC.

---
+++ iOSMovieObjc/MovieListTVC.h
@@ -0,0 +1,13 @@
+//
+//  MovieListTVC.h
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/12/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieListTVC : UITableViewController
+
+@end
