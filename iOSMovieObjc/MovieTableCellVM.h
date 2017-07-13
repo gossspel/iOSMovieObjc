0db8ee48fe7b998878f48c21c1e33459b3ffd6af
Sunny Chan <sunnyucb@gmail.com>
2017-07-12 21:13:06 -0700

    Added MovieTableCellVM.

---
+++ iOSMovieObjc/MovieTableCellVM.h
@@ -0,0 +1,13 @@
+//
+//  MovieTableCellVM.h
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/12/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovieTableCellVM : NSObject
+
+@end
---
+++ iOSMovieObjc/MovieTableCellVM.m
@@ -0,0 +1,13 @@
+//
+//  MovieTableCellVM.m
+//  iOSMovieObjc
+//
+//  Created by Sunny Chan on 7/12/17.
+//  Copyright © 2017 sunnystudio. All rights reserved.
+//
+
+#import "MovieTableCellVM.h"
+
+@implementation MovieTableCellVM
+
+@end
